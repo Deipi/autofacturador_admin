9cf9773521b5d9c76daaf0745ba12ce9f0809a12
Jose Mora Mejia <moraJose96@outlook.com>
2017-05-09 17:02:21 -0500

    design of the details page

--- src/components/DetalleFactura.js
+++ src/components/DetalleFactura.js
@@ -4,7 +4,54 @@ export class DetalleFactura extends Component {
 	render() {
 		return (
 			<div>
-				<h5>DetalleFactura</h5>
+				<div class="col-sm-6 col-sm-offset-3">
+					<h2>Detalle de Pago</h2>
+					<br/>
+					<div className="panel panel-default">
+						<div className="panel-bodys">
+							Resumen:
+						</div>
+					</div>
+					<table className="table">
+						<thead>
+							<tr>
+								<td class="col-sm-3"></td>
+								<td class="col-sm-9"></td>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td scope="row">Clave: </td>
+								<td>...</td>
+							</tr>
+							<tr>
+								<td scope="row">Estado:</td>
+								<td>...</td>
+							</tr>
+							<tr>
+								<td scope="row">Fecha de pago:</td>
+								<td>...</td>
+							</tr>
+							<tr>
+								<td scope="row">Total:</td>
+								<td>...</td>
+							</tr>
+							<th scope="row">
+								Concepto del pago:
+							</th>
+							<td>
+								<ul>
+									<li> A Taco de filete</li>
+									<li> Agua</li>
+									<li> Diabla</li>
+									<li> Quesadilla</li>
+								 </ul>
+							</td>
+						</tbody>
+					</table>
+				</div>
+				<hr/>
+				<h2 className="text-center">Comprobantes</h2>
 			</div>
 		);
 	}
