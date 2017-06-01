c18bd446cecaecd6b5551200308222d8201ab560
Jose Mora Mejia <moraJose96@outlook.com>
2017-06-01 14:01:02 -0500

    added condition to classify the type of ring

--- src/plugin/custom-row.jsx
+++ src/plugin/custom-row.jsx
@@ -10,6 +10,61 @@ import {
 import toggleRow from './actions';
 
 
+
+const Detail = ({ receptor, conceptos, created_at, total, uuid, recipient_code, state }) => {
+	if(	state==='I'){
+		return(
+			<div>
+				<hr/>
+				<h4>Resumen: </h4>
+				<strong className="span2 offset4">Clave: </strong> { recipient_code }
+				<br/>
+				<strong>Fecha de pago: </strong> { created_at }
+				<br/>
+				<strong>RFC: </strong> { receptor.get('rfc') }
+				<br/>
+				<strong>Total: </strong> { total }
+				<br/>
+				<strong>Folio fiscal: </strong> { uuid }
+				<br/>
+				<strong>Conceptos facturados: </strong> { conceptos.toJS().map(c => c['descripcion']).join(', ') }
+				<hr/>
+				<h2 className="text-center">Descargar</h2>
+				<div className="text-center">
+					<div className="text-center ">
+						<a className=" btn btn-primary btn-lg ladda-button" download data-style="zoom-in" data-resource="/selfinvoice/request/5cb06e88-cfa6-44e2-b1b9-6c51c954b830.pdf" role="button"><i className="fa fa-file-pdf-o fa-3x" /><br/>Archivo PDF</a>
+						<a className=" btn btn-primary btn-lg ladda-button" download data-style="zoom-in" data-resource="/selfinvoice/request/5cb06e88-cfa6-44e2-b1b9-6c51c954b830.zip" role="button"><i className="fa fa-file-zip-o fa-3x" /><br/>Archivo ZIP</a>
+						<a className=" btn btn-primary btn-lg ladda-button" download data-style="zoom-in" data-resource="/selfinvoice/request/5cb06e88-cfa6-44e2-b1b9-6c51c954b830.xml" role="button"><i className="fa fa-file-pdf-o fa-3x" /><br/>Archivo XML</a>
+					</div>
+					<hr/>
+					<div className="text-center">
+						<h2 className="text-center">Enviar por correo electronico</h2>
+						<Row className="col-sm-4">
+							<Col className="text-center">
+								<div className="form-group">
+									<div className="input-group push-8 offset-8" >
+										<span className="input-group-addon"><i className="fa fa-envelope fa-2x"/></span>
+										<input className="input-lg emailinput form-control required" id="email" name="email" placeholder="Proporcione su correo electronico" type="email" />
+									</div>
+								</div>
+							</Col>
+						</Row>
+					</div>
+				</div>
+				<Row className="offset-5">
+					<button className="btn btn-primary btn-lg ladda-button" data-invoice="/selfinvoice/invoice/email/5cb06e88-cfa6-44e2-b1b9-6c51c954b830/"
+					data-style="expand-left" type="submit" >Enviar correo</button>
+				</Row>
+			</div>
+
+			);
+	}
+	return(
+		<h1>cancelada</h1>
+	);
+}
+
+
 export default class CustomRow extends Component {
 	static contextTypes = {
 		router: React.PropTypes.object.isRequired,
@@ -33,7 +88,7 @@ export default class CustomRow extends Component {
 				columnIds,
 				style,
 				className,
-				rowData
+				rowData,
 			},
 		} = this;
 
@@ -43,9 +98,9 @@ export default class CustomRow extends Component {
 			receptor =rowData.get('receptor'),
 			total =rowData.get('total'),
 			uuid =rowData.get('uuid'),
-			conceptos =rowData.get('conceptos');
-
-		return (
+			conceptos =rowData.get('conceptos'),
+			state =rowData.get('state');
+ 		return (
 			<tbody>
 				<tr
 					key={ griddleKey }
@@ -67,46 +122,15 @@ export default class CustomRow extends Component {
 				<Collapse tag="tr" isOpen={ isOpen } className="data-detail">
 					<td colSpan="5" className="admin-table-edit">
 						<div>
-							<hr/>
-							<h4>Resumen: </h4>
-							<strong className="span2 offset4">Clave: </strong> { recipient_code }
-							<br/>
-							<strong>Fecha de pago: </strong> { created_at }
-							<br/>
-							<strong>RFC: </strong> { receptor.get('rfc') }
-							<br/>
-							<strong>Total: </strong> { total }
-							<br/>
-							<strong>Folio fiscal: </strong> { uuid }
-							<br/>
-							<strong>Conceptos facturados: </strong> { conceptos.toJS().map(c => c['descripcion']).join(', ') }
-							<hr/>
-							<h2 className="text-center">Descargar</h2>
-							<div className="text-center">
-								<div className="text-center ">
-									<a className=" btn btn-primary btn-lg ladda-button" download data-style="zoom-in" data-resource="/selfinvoice/request/5cb06e88-cfa6-44e2-b1b9-6c51c954b830.pdf" role="button"><i className="fa fa-file-pdf-o fa-3x" /><br/>Archivo PDF</a>
-									<a className=" btn btn-primary btn-lg ladda-button" download data-style="zoom-in" data-resource="/selfinvoice/request/5cb06e88-cfa6-44e2-b1b9-6c51c954b830.zip" role="button"><i className="fa fa-file-zip-o fa-3x" /><br/>Archivo ZIP</a>
-									<a className=" btn btn-primary btn-lg ladda-button" download data-style="zoom-in" data-resource="/selfinvoice/request/5cb06e88-cfa6-44e2-b1b9-6c51c954b830.xml" role="button"><i className="fa fa-file-pdf-o fa-3x" /><br/>Archivo XML</a>
-								</div>
-								<hr/>
-								<div className="text-center">
-									<h2 className="text-center">Enviar por correo electronico</h2>
-									<Row className="col-sm-4">
-										<Col className="text-center">
-											<div className="form-group">
-												<div className="input-group push-8 offset-8" >
-													<span className="input-group-addon"><i className="fa fa-envelope fa-2x"/></span>
-													<input className="input-lg emailinput form-control required" id="email" name="email" placeholder="Proporcione su correo electronico" type="email" />
-												</div>
-											</div>
-										</Col>
-									</Row>
-								</div>
-							</div>
-							<Row className="offset-5">
-								<button className="btn btn-primary btn-lg ladda-button" data-invoice="/selfinvoice/invoice/email/5cb06e88-cfa6-44e2-b1b9-6c51c954b830/"
-								data-style="expand-left" type="submit" >Enviar correo</button>
-							</Row>
+							<Detail
+								receptor = { receptor }
+								conceptos = { conceptos }
+								created_at = { created_at }
+								total = { total }
+								uuid = {uuid}
+								recipient_code = { recipient_code }
+								state={ state }
+							/>
 						</div>
 					</td>
 				</Collapse>
